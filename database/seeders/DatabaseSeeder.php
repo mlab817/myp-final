587b28edfa1a10bf4ec10456a48bc77217c6eece
Lester Bolotaolo <ipd-lester@Lesters-iMac.local>
2022-07-29 16:27:43 +0800

    changed app name and removed faker from user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +18,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-         \App\Models\User::factory()->create([
+         \App\Models\User::create([
              'name' => 'Test User',
              'email' => 'test@example.com',
+             'password' => Hash::make('password'),
          ]);
 
         $this->call(CommoditySeeder::class);
